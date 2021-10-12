63f075a5fa437968670219aabe2a30d223b5442c
Kuma-Rena <kuki.short25@gmail.com>
2021-10-12 18:01:32 +0900

    発展課題
    Magic関数を追加。それに合わせてStart関数にMPについての文を加筆。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,29 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+    private int hp = 100;          // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53; //マジックポイント
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+    public void Magic(int magic)
+    {
+        if (mp >= 5)//MPが5以上の場合
+        {
+            Debug.Log("魔法攻撃をした。\n残りのMPは" + mp);
+            this.mp -= magic;//MPを減らす
+        }
+        else
+        {
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
+
+    }
+
+}
+
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        //配列の初期化
-        int[] array = { 15, 23, 42, 56, 24 };
-        //配列の中を順番に表示
-        for(int a = 0; a < array.Length; a++)
-        {
-            Debug.Log(array[a]);
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+        for (int i = 0; i <= 10; i++) { //魔法用の関数を11回使う
+        lastboss.Magic(5);
         }
-        //さっきとは逆の順番で表示
-        for(int a = array.Length; a > 0; a--)
-        {
-            //array.Length=5なので-１する
-            Debug.Log(array[a-1]);
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
